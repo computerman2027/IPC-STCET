854d3c15476d5fde19452143f40c82162bbf0ec6
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2025-11-04 16:31:44 +0530

    PROGRAM 1 done

---
+++ pipe1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<unistd.h>
+
+
+int main()
+{
+    int pipefds[2];
+    int returnstatus;
+    char writemessage[2][20]={"Hi","Hello"};
+    char readmessage[20];
+
+    returnstatus=pipe(pipefds);
+
+    if(returnstatus ==-1)
+    {
+        printf("Unable to create a pipe\n");
+        return 1;
+    }
+
+    printf("Writing to pipe  = message 1 is %s\n",writemessage[0]);
+
+    write(pipefds[1],writemessage[0],sizeof(writemessage[0]));
+    read(pipefds[0],readmessage,sizeof(readmessage));
+
+    printf("Reading from pipe message 1 is %s\n",readmessage);
+
+    printf("Writing to pipe  = message 2 is %s\n",writemessage[1]);
+
+    write(pipefds[1],writemessage[1],sizeof(writemessage[1]));
+    read(pipefds[0],readmessage,sizeof(readmessage));
+
+    printf("Reading from pipe message 2 is %s\n",readmessage);
+
+    return 0;
+}
